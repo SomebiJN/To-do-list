23f12ec460b41c44cb50722ee0718639742314e5
Somebi Nwogu <sjnwogu@mun.ca>
2019-09-20 22:30:15 -0230

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+//triggered when user clicks on add button. Text gets added to do list.
+document.getElementById('addTask').addEventListener('click',function (ev) {
+    var value = document.getElementById('task').value;
+    if(value){
+
+    }
+});
