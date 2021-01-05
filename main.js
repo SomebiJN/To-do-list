2a162337573dd20d24d0e2c3a938f5c6e82f1556
Somebi Nwogu <sjnwogu@mun.ca>
2021-01-05 16:01:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 //triggered when user clicks on add button. Text gets added to do list.
 document.getElementById('addTask').addEventListener('click',function (ev) {
-    var value = document.getElementById('task').value;
-    if(value){
-
+    var li = document.createElement("li");
+    var invalue = document.getElementById('task').value;
+    var t = document.createTextNode(invalue);
+    if(invalue){
+        document.getElementById("mylist").appendChild(li);
     }
 });
